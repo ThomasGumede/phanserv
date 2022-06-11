da9bc2e657599301ca626508e61e5a39008358ce
Thomas <73006753+ThomasGumede@users.noreply.github.com>
2022-06-11 07:08:09 +0200

    Update ProjectCard.jsx

--- components/SharedComponents/ProjectCard.jsx
+++ components/SharedComponents/ProjectCard.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 function ProjectCard({title, location, img }) {
   return (
-    <div className="relative w-[330px] h-[450px] m-5 bg-color-h cursor-pointer overflow-hidden group duration-500">
+    <div className="relative w-[330px] h-[450px] bg-color-h cursor-pointer overflow-hidden group duration-500">
       <div className="w-full h-full overflow-hidden">
         <img src={img} alt="" className="w-full h-full duration-500 group-hover:scale-110 object-cover group-hover:opacity-50" />
         <Link href="/developing">
