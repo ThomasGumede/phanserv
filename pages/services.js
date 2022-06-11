ceab4bce123cfb49e2c10baafabe47a6ed65633d
Thomas <73006753+ThomasGumede@users.noreply.github.com>
2022-06-11 14:11:07 +0200

    Update services.js

--- pages/services.js
+++ pages/services.js
@@ -75,13 +75,13 @@ function services() {
                         Visit Us: 
                     </h1>
                     <p className="text-lg md:text-xl font-normal text-color-p">
-                        Gxulu Location,
+                        22 Gerald Spilkin Street,
                     </p>
                     <p className="text-lg md:text-xl font-normal text-color-p">
-                        Zithathele A.A, Libode,
+                        Ikhwezi Township, Umtata,
                     </p>
                     <p className="text-lg md:text-xl font-normal text-color-p">
-                         5160
+                         5099
                     </p>
                 </div>
             </div>
