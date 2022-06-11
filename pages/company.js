ddaca6d539060b29772a08d13602545e371a039c
Thomas <73006753+ThomasGumede@users.noreply.github.com>
2022-06-11 14:12:10 +0200

    Update company.js

--- pages/company.js
+++ pages/company.js
@@ -164,13 +164,13 @@ const company = () => {
                         Visit Us: 
                     </h1>
                     <p className="text-lg md:text-xl font-normal text-color-p">
-                        Gxulu Location,
+                        22 Gerald Spilkin Street,
                     </p>
                     <p className="text-lg md:text-xl font-normal text-color-p">
-                        Zithathele A.A, Libode,
+                        Ikhwezi Township, Umtata,
                     </p>
                     <p className="text-lg md:text-xl font-normal text-color-p">
-                         5160
+                         5099
                     </p>
                 </div>
             </div>
