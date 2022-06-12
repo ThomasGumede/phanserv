0b13b6458efd4d39cbd31cac0c3302e25c874969
Thomas Gumede <gumedethomas@outlook.com>
2022-06-12 10:26:15 +0200

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  purge: false,
   theme: {
     extend: {
       colors: {
